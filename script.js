124ebd8958203f3f07fec3451dc8466e2f81d56a
Nordin <128930417+nordin05@users.noreply.github.com>
2023-05-20 01:14:23 +0200

    add displayBooks function

--- script.js
+++ script.js
@@ -17,4 +17,33 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
+function displayBooks() {
+    const cards_container = document.querySelector(".cards-container");
+    cards_container.replaceChildren();
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const div = document.createElement("div");
+        div.className = "card";
+        cards_container.appendChild(div);
+
+        const h1 = document.createElement("h1");
+        h1.innerHTML = `${myLibrary[i].title}`;
+
+        const h2 = document.createElement("h2");
+        h2.innerHTML = `${myLibrary[i].author}`;
+
+        const p = document.createElement("p");
+        p.innerHTML = `${myLibrary[i].pages}`;
+
+        const p2 = document.createElement("p");
+        p2.innerHTML = `${myLibrary[i].read}`;
+
+        div.appendChild(h1);
+        div.appendChild(h2);
+        div.appendChild(p);
+        div.appendChild(p2);
+    }
+}
+
 addBookToLibrary();
+displayBooks();
